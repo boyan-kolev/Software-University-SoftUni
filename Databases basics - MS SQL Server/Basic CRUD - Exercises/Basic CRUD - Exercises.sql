94d36be27f23301dba7955055c2fa8a7b7cd7bd2
boyan <kolev0000@gmail.com>
2019-08-22 20:31:04 +0300

    Modify "Basic CRUD - Exercises.sql"

--- Databases basics - MS SQL Server/Basic CRUD - Exercises/Basic CRUD - Exercises.sql
+++ Databases basics - MS SQL Server/Basic CRUD - Exercises/Basic CRUD - Exercises.sql
@@ -105,3 +105,34 @@ SELECT Salary
 USE Diablo
 
 DROP DATABASE SoftUni
+
+--Part II – Queries for Geography Database
+--22. All Mountain Peaks
+USE Geography
+
+ SELECT PeakName
+   FROM Peaks
+ORDER BY PeakName
+
+--23. Biggest Countries by Population
+SELECT TOP(30) CountryName, [Population]
+  FROM Countries
+WHERE ContinentCode = 'EU'
+ORDER BY [Population] DESC, CountryName
+
+--24. *Countries and Currency (Euro / Not Euro)
+SELECT CountryName, CountryCode,
+CASE
+	WHEN CurrencyCode = 'EUR' THEN 'Euro'
+	ELSE 'Not Euro'
+END AS Currency
+  FROM Countries
+ORDER BY CountryName
+
+--Part III – Queries for Diablo Database
+--25. All Diablo Characters
+USE Diablo
+
+SELECT [Name]
+  FROM Characters
+ORDER BY [Name]
