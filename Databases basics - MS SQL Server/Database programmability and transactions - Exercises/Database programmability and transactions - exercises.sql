f6af53ab5257402b299b366db323ee9aca634178
boyan <kolev0000@gmail.com>
2019-10-05 21:26:28 +0300

    Add Bank-Database.sql and modify Database programmability and transactions - exercises.sql

--- Databases basics - MS SQL Server/Database programmability and transactions - Exercises/Database programmability and transactions - exercises.sql
+++ Databases basics - MS SQL Server/Database programmability and transactions - Exercises/Database programmability and transactions - exercises.sql
@@ -61,3 +61,117 @@ BEGIN
 END
 GO
 
+--Problem 6. Employees by Salary Level
+CREATE PROC usp_EmployeesBySalaryLevel @LevelOfSalary VARCHAR(15)
+AS
+SELECT FirstName AS [First Name],
+		LastName AS [Last Name]
+FROM Employees
+WHERE dbo.ufn_GetSalaryLevel(Salary) = @LevelOfSalary
+GO
+
+--Problem 7. Define Function
+CREATE FUNCTION ufn_IsWordComprised(@setOfLetters VARCHAR(MAX), @word VARCHAR(MAX))
+RETURNS BIT
+BEGIN
+	DECLARE @count INT = 1
+	WHILE(@count <= LEN(@word))
+	BEGIN
+		DECLARE @currentLetter CHAR(1) = SUBSTRING(@word, @count, 1)
+		DECLARE @charIdex INT = CHARINDEX(@currentLetter, @setOfLetters)
+
+		IF(@charIdex = 0)
+		BEGIN
+			RETURN 0
+		END
+		SET @count += 1
+	END
+	RETURN 1
+END
+GO
+
+--Problem 8. * Delete Employees and Departments
+CREATE PROC usp_DeleteEmployeesFromDepartment (@departmentId INT)
+AS
+ALTER TABLE Departments
+ALTER COLUMN ManagerID INT
+
+UPDATE Departments
+SET ManagerID = NULL
+WHERE DepartmentID = @departmentId
+
+UPDATE Employees
+SET ManagerID = NULL
+WHERE ManagerID IN (SELECT EmployeeID FROM Employees WHERE DepartmentID = @departmentId)
+
+DELETE FROM EmployeesProjects
+WHERE EmployeeID IN (SELECT EmployeeID FROM Employees WHERE DepartmentID = @departmentId)
+
+DELETE FROM Employees
+WHERE DepartmentID = @departmentId
+
+DELETE FROM Departments
+WHERE DepartmentID = @departmentId
+
+SELECT COUNT(*)
+FROM Employees
+WHERE DepartmentID = @departmentId
+GO
+
+--Part 2. Queries for Bank Database
+--Problem 9. Find Full Name
+USE Bank
+GO
+
+CREATE PROCEDURE usp_GetHoldersFullName
+AS
+SELECT CONCAT(FirstName, ' ', LastName)
+FROM AccountHolders
+GO
+
+--Problem 10. People with Balance Higher Than
+CREATE PROC usp_GetHoldersWithBalanceHigherThan (@InputMoney MONEY)
+AS
+SELECT t.FirstName AS [First Name],
+		t.LastName AS [Last Name]
+FROM (SELECT ah.Id, 
+	 		ah.FirstName,
+	 		ah.LastName,
+	 		SUM(a.Balance) AS TotalBalance
+	 FROM AccountHolders AS ah
+	 JOIN Accounts AS a
+	 ON a.AccountHolderId = ah.Id
+	 GROUP BY ah.Id, ah.FirstName, ah.LastName
+	 HAVING SUM(a.Balance) > @InputMoney) AS t
+ORDER BY t.FirstName, t.LastName
+GO
+
+--Problem 11. Future Value Function
+CREATE FUNCTION ufn_CalculateFutureValue (@InitialSum DECIMAL(18, 4), @YearlyInterestRate FLOAT, @NumberOfYears INT)
+RETURNS DECIMAL(18, 4)
+BEGIN
+	DECLARE @futureValue DECIMAL(18, 4)
+	SET @futureValue = @InitialSum * POWER((1 + @YearlyInterestRate), @NumberOfYears)
+	RETURN @futureValue
+END
+GO
+
+--Problem 12. Calculating Interest
+CREATE PROC usp_CalculateFutureValueForAccount (@AccountID INT, @InterestRate FLOAT)
+AS
+SELECT t.Id AS [Account id],
+		t.FirstName AS [First Name],
+		t.LastName AS [Last Name],
+		t.TotalBalance AS [Current Balance],
+		dbo.ufn_CalculateFutureValue(t.TotalBalance, @InterestRate, 5) AS [Balance in 5 years]
+FROM (SELECT a.Id,
+	 		FirstName,
+	 		ah.LastName,
+	 		SUM(a.Balance) AS TotalBalance
+	 FROM AccountHolders AS ah
+	 JOIN Accounts AS a
+	 ON a.AccountHolderId = ah.Id
+	 WHERE a.Id = @AccountID
+	 GROUP BY a.Id, FirstName, ah.LastName) AS t
+
+
