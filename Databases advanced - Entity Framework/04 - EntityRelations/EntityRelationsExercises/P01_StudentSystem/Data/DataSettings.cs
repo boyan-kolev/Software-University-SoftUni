1919287288cf840326446f6cee9db754a9a5b6da
boyan <kolev0000@gmail.com>
2019-11-14 19:42:47 +0200

    Modify EntityRelations - Exercises

--- Databases advanced - Entity Framework/04 - EntityRelations/EntityRelationsExercises/P01_StudentSystem/Data/DataSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace P01_StudentSystem.Data
-{
-    public static class DataSettings
-    {
-        public const string DefaultConnection = "Server = DESKTOP-PHQG6TO\\SQLEXPRESS; Database = StudentSystem; Integrated Security = True";
-    }
-}
